d01eb570e66daf0a838cd9d25a55818afc24520c
Your Name <you@example.com>
2025-05-29 00:05:55 +0200

    Crate basic layout

--- medicamento/wwwroot/js/site.js
+++ medicamento/wwwroot/js/site.js
@@ -1,4 +1,65 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿/*!
+    * Start Bootstrap - SB Admin v7.0.7 (https://startbootstrap.com/template/sb-admin)
+    * Copyright 2013-2023 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
+    */
+// 
+// Scripts
+// 
 
-// Write your JavaScript code.
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+    // Close side navigation when width < LG
+    const sidenavContent = document.body.querySelector('#layoutSidenav_content');
+    if (sidenavContent) {
+        sidenavContent.addEventListener('click', event => {
+            const BOOTSTRAP_LG_WIDTH = 992;
+            if (window.innerWidth >= 992) {
+                return;
+            }
+            if (document.body.classList.contains("sb-sidenav-toggled")) {
+                document.body.classList.toggle("sb-sidenav-toggled");
+            }
+        });
+    }
+
+});
+
+
+
+(function ($) {
+    "use strict"; //
+
+    $(document).on('scroll', function () {
+        var scrollDistance = $(this).scrollTop();
+        if (scrollDistance > 100) {
+            $('.scroll-to-top').fadeIn();
+        } else {
+            $('.scroll-to-top').fadeOut();
+        }
+    });
+
+    // Smooth scrolling using jQuery easing
+    $(document).on('click', 'a.scroll-to-top', function (e) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top)
+        }, 1000, 'easeInOutExpo');
+        e.preventDefault();
+    });
+
+})(jQuery); // End of use strict
